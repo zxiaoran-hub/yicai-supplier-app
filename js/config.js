eb550e67d6cc6f220e94f941478b7aa2fe38d043
yicai-deploy <deploy@yicai.app>
2026-07-30 09:59:55 +0800

    chore: 切换到个人账号Supabase配置

--- js/config.js
+++ js/config.js
@@ -1,3 +1,3 @@
 // 异采 YiCai 供应商端 Supabase 配置
-const SUPABASE_URL = 'https://spb-hifzhn3j7pqw971s.supabase.opentrust.net';
-const SUPABASE_ANON_KEY = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJyb2xlIjoiYW5vbiIsInJlZiI6InNwYi1oaWZ6aG4zajdwcXc5NzFzIiwiaXNzIjoic3VwYWJhc2UiLCJpYXQiOjE3ODUzMjUxODAsImV4cCI6MjEwMDkwMTE4MH0.odNQDfrqf2AmXKkEIrAfCSpkS6qNZ3GoADUD8IDWBDs';
+const SUPABASE_URL = 'https://spb-bv8ktmo1ar03vmqh.supabase.opentrust.net';
+const SUPABASE_ANON_KEY = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJyb2xlIjoiYW5vbiIsInJlZiI6InNwYi1idjhrdG1vMWFyMDN2bXFoIiwiaXNzIjoic3VwYWJhc2UiLCJpYXQiOjE3ODUzNzY1NjIsImV4cCI6MjEwMDk1MjU2Mn0.v9z67DVkmzN-WaTF69s1Qs7y-0bwQ3HQP3yX81J0XQg';
