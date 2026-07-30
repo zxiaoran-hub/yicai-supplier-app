47dc48270fddcecb29002789f902ff1688c2153c
yicai-deploy <deploy@yicai.app>
2026-07-30 10:08:08 +0800

    chore: 切换到新Supabase项目 spb-m06skr4cysol4lwz

--- js/config.js
+++ js/config.js
@@ -1,3 +1,3 @@
 // 异采 YiCai 供应商端 Supabase 配置
-const SUPABASE_URL = 'https://spb-bv8ktmo1ar03vmqh.supabase.opentrust.net';
-const SUPABASE_ANON_KEY = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJyb2xlIjoiYW5vbiIsInJlZiI6InNwYi1idjhrdG1vMWFyMDN2bXFoIiwiaXNzIjoic3VwYWJhc2UiLCJpYXQiOjE3ODUzNzY1NjIsImV4cCI6MjEwMDk1MjU2Mn0.v9z67DVkmzN-WaTF69s1Qs7y-0bwQ3HQP3yX81J0XQg';
+const SUPABASE_URL = 'https://spb-m06skr4cysol4lwz.supabase.opentrust.net';
+const SUPABASE_ANON_KEY = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJyb2xlIjoiYW5vbiIsInJlZiI6InNwYi1tMDZza3I0Y3lzb2w0bHd6IiwiaXNzIjoic3VwYWJhc2UiLCJpYXQiOjE3ODUzNzcwNjIsImV4cCI6MjEwMDk1MzA2Mn0.2OO2jmTetq6vOE4xTRruNMXVUI89ATMIStpIl4ul3kI';
